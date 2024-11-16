d9fd944c7de2955c2726a93c753a9733e66aefe3
Kostia06 <ilnkostia@gmail.com>
2024-11-16 00:53:33 -0700

    added shadcn and did routing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,17 @@
 "use client";
 import React from "react";
 import { useGlobalContext } from "@/containers/GlobalContext";
-
+import { Input } from "@/ui/input"
 
 export default function Home() {
-    const { user, userSet } = useGlobalContext();
+    const { user, setUser } = useGlobalContext();
     return (
-        <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full flex flex-col items-center justify-center">
             <h1 className="text-sky-500 font-bold text-3xl">
                 {user}
             </h1>
+            <Input onChange={(e) => setUser(e.target.value)} className="w-62" placeholder="User" />
+
         </div>
     );
 }
