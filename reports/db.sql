157c3f7ed1d268c80a6505eb2a444fdbba17cb81
Kostia06 <ilnkostia@gmail.com>
2024-11-16 14:28:20 -0700

    finished the sql

--- reports/db.sql
+++ reports/db.sql
@@ -214,7 +214,7 @@ WHERE ED_ID = <ID>;
 -- Update SocialLinks information
 UPDATE SocialLinks
 SET SL_Link = <Link>,
-    SL_Type = <Type>
+    SL_Type = <Type>,
 WHERE SL_ID = <ID>;
 
 --  Get every single project based on the Client
@@ -362,3 +362,386 @@ WHERE E_ID IN (
     FROM EntrepreneurCompany
     WHERE EC_Name LIKE <Name>
 );
+
+
+
+-- Update Client information
+UPDATE Client
+SET C_Username = <Username>,
+    C_Password = <Password>,
+    C_Email = <Email>,
+    C_FirstName = <FirstName>,
+    C_LastName = <LastName>,
+    C_MiddleName = <MiddleName>,
+    C_Education = <Education>
+WHERE C_ID = <ID>;
+
+-- Update Education information
+UPDATE Education
+SET ED_School = <School>,
+    ED_Degree = <Degree>,
+    ED_Field = <Field>,
+    ED_StartDate = <StartDate>,
+    ED_EndDate = <EndDate>
+WHERE ED_ID = <ID>;
+
+
+-- Update SocialLinks information
+UPDATE SocialLinks
+SET SL_Link = <Link>,
+    SL_Type = <Type>,
+WHERE SL_ID = <ID>;
+
+-- Update Task information
+UPDATE Task
+SET 
+    T_Description = <Description>,
+    T_StartDate = <StartDate>,
+    T_EndDate = <EndDate>,
+    T_Status = <Status>
+WHERE 
+    T_ID = <ID>;
+
+-- Update Message details
+UPDATE Message
+SET 
+    M_Content = <Content>,
+    M_DateSent = <DateSent>
+WHERE 
+    M_ID = <ID>;
+
+-- Update Post information
+UPDATE Post
+SET 
+    P_Title = <Title>,
+    P_Content = <Content>,
+    P_DatePosted = <DatePosted>
+WHERE 
+    P_ID = <ID>;
+
+
+--  Get every single project based on the Client
+SELECT * 
+FROM Project
+WHERE PR_ID IN (
+    SELECT PR_ID 
+    FROM JoinCreate 
+    WHERE C_ID = <ID>
+);
+
+-- Get every single client based on the project
+SELECT *
+FROM Client
+WHERE C_ID IN (
+    SELECT C_ID 
+    FROM JoinCreate
+    WHERE PR_ID = <ID>
+);
+
+-- Get the project based on the Post
+SELECT *
+FROM Project
+WHERE PR_ID IN (
+    SELECT PR_ID
+    FROM LinkUp
+    WHERE P_ID = <ID>
+);
+
+-- Get the post based on the Project
+SELECT *
+FROM Post
+WHERE P_ID IN (
+    SELECT P_ID
+    FROM LinkUp
+    WHERE PR_ID = <ID>
+);
+
+-- Get Comments based on the PostTag
+SELECT *
+FROM Comment
+WHERE P_ID IN (
+    SELECT P_ID
+    FROM PostTag
+    WHERE TA_ID = <ID>
+);
+
+-- Get comments based on comments
+SELECT *
+FROM Comment
+WHERE CO_SuperID = <ID>;
+
+-- Get tag based on Post 
+SELECT *
+FROM Tag
+WHERE TA_ID IN (
+    SELECT TA_ID
+    FROM PostTag
+    WHERE P_ID = <ID>
+);
+
+-- Get tag based on ProjectTag
+SELECT *
+FROM Tag
+WHERE TA_ID IN (
+    SELECT TA_ID
+    FROM ProjectTag
+    WHERE PR_ID = <ID>
+);
+
+-- Get tag based on tag details type 
+SELECT *
+FROM Tag
+WHERE TA_ID IN (
+    SELECT TA_ID
+    FROM TagDetails
+    WHERE TD_Type = <TYPE>
+);
+
+-- Get tag details based on TagDetails
+SELECT *
+FROM TagDetails
+WHERE TA_ID = <ID>;
+
+-- Get task based on Project
+SELECT *
+FROM Task
+WHERE PR_ID = <ID>;
+
+-- Get tasks based on client from notify
+SELECT *
+FROM Task
+WHERE TA_ID IN (
+    SELECT TA_ID
+    FROM Notify
+    WHERE C_ID = <ID>
+);
+
+-- Get tasks based on status 
+SELECT *
+FROM Task
+WHERE T_Status = <Status>;
+
+-- Get messages based on client 
+SELECT *
+FROM Message
+WHERE M_ID IN (
+    SELECT M_ID
+    FROM Send
+    WHERE C_ID = <ID>
+);
+
+-- Get all of the E-Company Names
+SELECT EC_Name 
+FROM EntrepreneurCompany;
+
+-- Get all of the Developer Technologies
+SELECT DT_Name
+FROM DeveloperTechnology;
+
+-- Get all of the Admins based on username
+SELECT *
+FROM Admins
+WHERE A_Username LIKE <Username>;
+
+-- Get all of the Clients based on username 
+SELECT *
+FROM Client
+WHERE C_Username LIKE <Username>;
+
+-- Get all of the clients based on education 
+SELECT *
+FROM Client
+WHERE C_ID IN (
+    SELECT C_ID
+    FROM Education
+    WHERE ED_School LIKE <School>
+);
+
+-- Get all of the Entrepreneurs based on company Names
+SELECT *
+FROM Entrepreneur
+WHERE E_ID IN (
+    SELECT E_ID
+    FROM EntrepreneurCompany
+    WHERE EC_Name LIKE <Name>
+);
+
+-- Insert a new Client
+INSERT INTO Client (C_Name, C_Email, C_PhoneNumber, C_Address)
+VALUES (<Name>, <Email>, <PhoneNumber>, <Address>);
+
+-- Insert a new Admin
+INSERT INTO Admin (A_Name, A_Email, A_PhoneNumber)
+VALUES (<Name>, <Email>, <PhoneNumber>);
+
+-- Insert a new Project
+INSERT INTO Project (PR_Name, PR_Description, PR_StartDate, PR_EndDate)
+VALUES (<ProjectName>, <ProjectDescription>, <StartDate>, <EndDate>);
+
+-- Insert a new Task
+INSERT INTO Task (T_Description, T_StartDate, T_EndDate, T_Status, D_ID)
+VALUES (<Description>, <StartDate>, <EndDate>, <Status>, <DeveloperID>);
+
+-- Insert a new Developer
+INSERT INTO Developer (D_Name, D_Email, D_SkillSet)
+VALUES (<Name>, <Email>, <SkillSet>);
+
+-- Insert a new Entrepreneur
+INSERT INTO Entrepreneur (E_Name, E_Email, E_Company)
+VALUES (<Name>, <Email>, <Company>);
+
+-- Insert a new Tag
+INSERT INTO Tag (TA_Name)
+VALUES (<TagName>);
+
+-- Insert a new Message
+INSERT INTO Message (M_Content, M_DateSent, M_SenderID, M_ReceiverID)
+VALUES (<Content>, <DateSent>, <SenderID>, <ReceiverID>);
+
+-- Insert a new Post
+INSERT INTO Post (P_Title, P_Content, P_DatePosted, C_ID)
+VALUES (<Title>, <Content>, <DatePosted>, <ClientID>);
+
+-- Insert a new Comment
+INSERT INTO Comment (CO_Content, CO_DatePosted, P_ID, C_ID)
+VALUES (<Content>, <DatePosted>, <PostID>, <ClientID>);
+
+-- Insert a new Project-Client association
+INSERT INTO JoinCreate (PR_ID, C_ID)
+VALUES (<ProjectID>, <ClientID>);
+
+-- Insert a new Project-Admin association
+INSERT INTO ProjectAdmin (PR_ID, A_ID)
+VALUES (<ProjectID>, <AdminID>);
+
+-- Insert a new Post-Tag association
+INSERT INTO PostTag (P_ID, TA_ID)
+VALUES (<PostID>, <TagID>);
+
+-- Insert a new Post-Client association
+INSERT INTO PostClient (P_ID, C_ID)
+VALUES (<PostID>, <ClientID>);
+
+-- Insert a new Developer-Client association
+INSERT INTO DeveloperClient (D_ID, C_ID)
+VALUES (<DeveloperID>, <ClientID>);
+
+-- Insert a new Entrepreneur-Developer association
+INSERT INTO EntrepreneurDeveloper (E_ID, D_ID)
+VALUES (<EntrepreneurID>, <DeveloperID>);
+
+-- Insert a new Task-Client association
+INSERT INTO TaskClient (T_ID, C_ID)
+VALUES (<TaskID>, <ClientID>);
+
+-- Insert a new Send association for messages
+INSERT INTO Send (M_ID, C_ID)
+VALUES (<MessageID>, <ClientID>);
+
+-- Insert a new Notification
+INSERT INTO Notify (C_ID, A_ID, N_Message, N_Date)
+VALUES (<ClientID>, <AdminID>, <Message>, <Date>);
+
+-- Insert a new Task-Tag association
+INSERT INTO TaskTag (T_ID, TA_ID)
+VALUES (<TaskID>, <TagID>);
+
+-- Delete a Client
+DELETE FROM Client
+WHERE C_ID = <ID>;
+
+-- Delete an Admin
+DELETE FROM Admin
+WHERE A_ID = <ID>;
+
+-- Delete a Project
+DELETE FROM Project
+WHERE PR_ID = <ID>;
+
+-- Delete a Task
+DELETE FROM Task
+WHERE T_ID = <ID>;
+
+-- Delete a Developer
+DELETE FROM Developer
+WHERE D_ID = <ID>;
+
+-- Delete an Entrepreneur
+DELETE FROM Entrepreneur
+WHERE E_ID = <ID>;
+
+-- Delete a Tag
+DELETE FROM Tag
+WHERE TA_ID = <ID>;
+
+-- Delete a Message
+DELETE FROM Message
+WHERE M_ID = <ID>;
+
+-- Delete a Post
+DELETE FROM Post
+WHERE P_ID = <ID>;
+
+-- Delete a Comment
+DELETE FROM Comment
+WHERE CO_ID = <ID>;
+
+-- Delete a Project-Client association
+DELETE FROM JoinCreate
+WHERE PR_ID = <ProjectID> AND C_ID = <ClientID>;
+
+-- Delete a Project-Admin association
+DELETE FROM ProjectAdmin
+WHERE PR_ID = <ProjectID> AND A_ID = <AdminID>;
+
+-- Delete a Post-Tag association
+DELETE FROM PostTag
+WHERE P_ID = <PostID> AND TA_ID = <TagID>;
+
+-- Delete a Post-Client association
+DELETE FROM PostClient
+WHERE P_ID = <PostID> AND C_ID = <ClientID>;
+
+-- Delete a Developer-Client association
+DELETE FROM DeveloperClient
+WHERE D_ID = <DeveloperID> AND C_ID = <ClientID>;
+
+-- Delete an Entrepreneur-Developer association
+DELETE FROM EntrepreneurDeveloper
+WHERE E_ID = <EntrepreneurID> AND D_ID = <DeveloperID>;
+
+-- Delete a Task-Client association
+DELETE FROM TaskClient
+WHERE T_ID = <TaskID> AND C_ID = <ClientID>;
+
+-- Delete a Send association for a message
+DELETE FROM Send
+WHERE M_ID = <MessageID> AND C_ID = <ClientID>;
+
+-- Delete a Notification
+DELETE FROM Notify
+WHERE N_ID = <NotificationID>;
+
+-- Delete a Task-Tag association
+DELETE FROM TaskTag
+WHERE T_ID = <TaskID> AND TA_ID = <TagID>;
+
+-- Delete a client and all related associations (such as tasks, posts, etc.)
+DELETE FROM Client
+WHERE C_ID = <ID>;
+
+-- Delete a project and all related associations (such as project-client, project-admin, etc.)
+DELETE FROM Project
+WHERE PR_ID = <ID>;
+
+-- Delete a developer and all related associations (such as task-client, developer-client, etc.)
+DELETE FROM Developer
+WHERE D_ID = <ID>;
+
+-- Delete a tag and all related associations (such as post-tag, task-tag)
+DELETE FROM Tag
+WHERE TA_ID = <ID>;
+
+-- Delete a message and all related associations (such as send messages)
+DELETE FROM Message
+WHERE M_ID = <ID>;
